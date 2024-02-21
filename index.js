61bffac3a7a7b79e81d341180630d68f06e7eb59
Alessandro <alessandroRetuerto@hotmail.com>
2024-02-21 14:29:57 -0500

    add excute bd

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
-
 const app = express();
 const PORT = 5000;
-
+const { executeDB } = require("./db");
 const JWT_SECRET = "secret"; // Clave secreta para firmar los tokens JWT
 
 // Middleware
@@ -13,54 +12,67 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Base de datos ficticia de usuarios
-const users = [];
 
-// Ruta para registro de usuario
 app.post("/register", async (req, res) => {
   try {
-    // Hash de la contraseña
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    // Creación del usuario
+
+    const findUser = await executeDB("SELECT * FROM users WHERE email = ?", [
+      req.body.email,
+    ]);
+
+    if (findUser[0]) {
+      res.status(400).json({
+        resp: "Usuario ya existe",
+      });
+      return;
+    }
     const user = {
       name: req.body.name,
       email: req.body.email,
       password: hashedPassword,
     };
-    // Agregar usuario a la lista
-    users.push(user);
-    res.status(200).json({
-      resp: "Creado con exito",
-    });
-  } catch {
-    res.status(500).json({
-      resp: "Error no creado",
-    });
+    const registerUser = executeDB("INSERT INTO users SET ?", user);
+
+    if (registerUser) {
+      console.log("Usuario registrado con éxito en la base de datos");
+      res.status(200).json({
+        resp: "Creado con exito",
+      });
+    }
+  } catch (error) {
+    console.error(err);
+    res.status(500).send("Error al registrar usuario en la base de datos");
   }
 });
 
 // Ruta para inicio de sesión
 app.post("/login", async (req, res) => {
-  const user = users.find((user) => user.email === req.body.email);
-  if (user == null) {
-    return res.status(400).send("Usuario no encontrado");
-  }
+  const email = req.body.email;
+  const password = req.body.password;
   try {
-    if (await bcrypt.compare(req.body.password, user.password)) {
-      /*     res.status(200).json({
-        resp: "Inicio de sesión exitoso",
-      }); */
+    const loginUser = await executeDB("SELECT * FROM users WHERE email = ?", [
+      email,
+    ]);
+
+    if (loginUser.length === 0) {
+      res.status(401).send("Credenciales incorrectas");
+    }
+
+    const user = loginUser[0];
+
+    if (await bcrypt.compare(password, user.password)) {
+      // Generar token JWT
       const token = jwt.sign({ email: user.email }, JWT_SECRET);
-      res.status(200).json({ resp: token });
+      res.json({ token });
     } else {
-      res.status(400).json({
+      res.status(401).json({
         resp: "Credenciales incorrectas",
       });
     }
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      resp: "Error del servidor",
-    });
+  } catch (error) {
+    console.error(err);
+    res.status(500).send("Error al buscar usuario en la base de datos");
   }
 });
 
