1ba964927bbf6a268c26bdf1a38e5fc5a91295cc
Alessandro <alessandroRetuerto@hotmail.com>
2024-02-21 20:41:38 -0500

    add env

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql");
+require("dotenv").config();
 
 const db = mysql.createPool({
-  host: "quo.cheuuoo0eeln.us-east-1.rds.amazonaws.com",
-  user: "root",
-  password: "Alessandro123!",
-  database: "quo",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 db.getConnection((err) => {
   if (err) {
